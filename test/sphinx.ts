3e5ee3087d8df6ceb348e7c9c4c0467ff969651b
Arik Sosman <git@arik.io>
2019-11-19 17:54:49 -0800

    allow layer peeling with either hop private key or the shared secret

--- test/sphinx.ts
+++ test/sphinx.ts
@@ -56,7 +56,10 @@ describe('Sphinx Tests', () => {
 
 		const peel0 = sphinx.peel({hopPrivateKey: hopPrivateKeys[0], associatedData});
 		const peel1 = peel0.sphinx.peel({hopPrivateKey: hopPrivateKeys[1], associatedData});
-		const peel2 = peel1.sphinx.peel({hopPrivateKey: hopPrivateKeys[2], associatedData});
+		const peel2 = peel1.sphinx.peel({
+			sharedSecret: Buffer.from('3a6b412548762f0dbccce5c7ae7bb8147d1caf9b5471c34120b30bc9c04891cc', 'hex'),
+			associatedData
+		});
 		const peel3 = peel2.sphinx.peel({hopPrivateKey: hopPrivateKeys[3], associatedData});
 		const peel4 = peel3.sphinx.peel({hopPrivateKey: hopPrivateKeys[4], associatedData});
 		assert.isNull(peel4.sphinx);
